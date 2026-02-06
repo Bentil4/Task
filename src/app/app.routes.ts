db15d7f223140c2654b9e9e4deb6f39ab7a9b6ef
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-02-06 00:00:36 +0000

    Add new assets and update routing configuration
    - Added various SVG icons for UI components including check, chevron, and theme icons.
    - Introduced favicon for the application.
    - Created logo assets for both dark and light themes.
    - Added mobile logo asset.
    - Initialized data.json with board and task structure for project management.
    - Updated app routing to include layout, login, settings, and a 404 page not found component.
    - Modified tsconfig.spec.json to include layout component's spec file for testing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { layout } from './pages/layout/layout';
+import { Login } from './pages/login/login';
+import { Settings } from './pages/settings/settings';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: layout,
+    title: 'Home',
+  },
+  {
+    path: 'settings',
+    component: Settings,
+    title: 'Settings',
+  },
+  {
+    path: 'login',
+    component: Login,
+    title: 'Login',
+  },
+  {
+    path: '**',
+    component: PageNotFound,
+  },
+];
