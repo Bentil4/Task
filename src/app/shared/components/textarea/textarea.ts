9181c34a86229610b7cb62c1e1935f7b3d75ade0
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-02-13 17:05:04 +0000

    feat: add textarea component with HTML, CSS, and TypeScript implementation

---
+++ src/app/shared/components/textarea/textarea.ts
@@ -0,0 +1,23 @@
+import { Component, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-textarea',
+  templateUrl: './textarea.html',
+  styleUrl: './textarea.css',
+  imports: [FormsModule],
+})
+export class TextareaComponent {
+  label = input<string>('');
+  placeholder = input<string>('');
+  value = input<string>('');
+  rows = input<number>(4);
+  required = input<boolean>(false);
+  disabled = input<boolean>(false);
+  
+  valueChange = output<string>();
+
+  onValueChange(newValue: string) {
+    this.valueChange.emit(newValue);
+  }
+}
