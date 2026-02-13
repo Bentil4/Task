d0f765012920c687d8abbea3619d52ffc1ead709
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-02-13 17:04:52 +0000

    feat: implement select component with HTML, CSS, and TypeScript

---
+++ src/app/shared/components/select/select.ts
@@ -0,0 +1,27 @@
+import { Component, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+export interface SelectOption {
+  label: string;
+  value: string;
+}
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.html',
+  styleUrl: './select.css',
+  imports: [FormsModule],
+})
+export class SelectComponent {
+  label = input<string>('');
+  options = input<SelectOption[]>([]);
+  value = input<string>('');
+  required = input<boolean>(false);
+  disabled = input<boolean>(false);
+  
+  valueChange = output<string>();
+
+  onValueChange(newValue: string) {
+    this.valueChange.emit(newValue);
+  }
+}
