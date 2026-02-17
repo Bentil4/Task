bc422ff9e9534ee0865626e024cfce4fc59e9f28
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-02-17 07:49:20 +0000

    separate html and css code from show-sidebar-components

--- src/app/shared/components/show-sidebar-button/show-sidebar-button.component.ts
+++ src/app/shared/components/show-sidebar-button/show-sidebar-button.component.ts
@@ -2,42 +2,8 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'button[app-show-sidebar-button]',
-  template: `
-    <svg width="16" height="11" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-      <path
-        d="M15.815 4.434A9.055 9.055 0 0 0 8 0 9.055 9.055 0 0 0 .185 4.434a1.333 1.333 0 0 0 0 1.354A9.055 9.055 0 0 0 8 10.222c3.33 0 6.25-1.777 7.815-4.434a1.333 1.333 0 0 0 0-1.354ZM8 8.89A3.776 3.776 0 0 1 4.222 5.11 3.776 3.776 0 0 1 8 1.333a3.776 3.776 0 0 1 3.778 3.778A3.776 3.776 0 0 1 8 8.89Zm2.889-3.778a2.889 2.889 0 1 1-5.438-1.36 1.19 1.19 0 1 0 1.19-1.189H6.64a2.889 2.889 0 0 1 4.25 2.549Z"
-        fill="currentColor"
-      />
-    </svg>
-  `,
-  styles: `
-    :host {
-      position: fixed;
-      bottom: 2rem;
-      left: 0;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 3.5rem;
-      height: 3rem;
-      border: none;
-      border-radius: 0 100px 100px 0;
-      background-color: var(--color-primary);
-      color: var(--color-white);
-      cursor: pointer;
-      transition: background-color 150ms ease;
-      z-index: 50;
-    }
-
-    :host:hover {
-      background-color: var(--color-primary-hover);
-    }
-
-    :host:focus-visible {
-      outline: 2px solid var(--color-primary-hover);
-      outline-offset: 2px;
-    }
-  `,
+  templateUrl: './show-sidebar-button.component.html',
+  styleUrl: './show-sidebar-button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
