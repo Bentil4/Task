de0ea0921c6bdc0b83ad47b148405c1a0ca877d3
Bentil4 <bentilfrederick4@gmail.com>
2026-03-05 12:11:10 +0000

    feat(config): integrate ngrx store, effects, and devtools providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
+import { boardReducer } from './core/store/board/reducer/board.reducer';
+import { BoardEffects } from './core/store/board/effects/board.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideStore({ board: boardReducer }),
+    provideEffects([BoardEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: false })
+  ],
 };
